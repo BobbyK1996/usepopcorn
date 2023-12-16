f6b7540878643c1d8c36d7b099f71f63b3b66e3d
BobbyK1996 <bobbykamal@hotmail.co.uk>
2023-12-16 01:03:00 +0000

    Converted NavBar and ListBox to use Component Composition using the children parameter. Used this to replace extensive prop drilling

--- src/App.js
+++ src/App.js
@@ -56,20 +56,27 @@ const App = () => {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 };
 
 //NAVBAR
 //Structural
-const NavBar = ({ movies }) => {
+const NavBar = ({ children }) => {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 };
@@ -110,18 +117,13 @@ const NumResults = ({ movies }) => {
 
 //MAIN
 //Structural
-const Main = ({ movies }) => {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+const Main = ({ children }) => {
+  return <main className="main">{children}</main>;
 };
 
 //LEFT HAND SIDE - LIST BOX
 //Stateful
-const ListBox = ({ movies }) => {
+const ListBox = ({ children }) => {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -132,7 +134,7 @@ const ListBox = ({ movies }) => {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 };
