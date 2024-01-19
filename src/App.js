2388aa80df1b5712985f1e881c33db5e3e3af037
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-01-19 14:49:32 +0000

    Added useEffect using an anonymous function, to load the data call when the application mounts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const tempMovieData = [
   {
@@ -57,10 +57,12 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=godzilla`)
-    .then((res) => res.json())
-    .then((data) => console.log(data.Search))
-    .catch((err) => console.log('Error:', err));
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=godzilla`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search))
+      .catch((err) => console.log('Error:', err));
+  }, []);
 
   return (
     <>
