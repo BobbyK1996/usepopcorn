4625ec4c9a2710d66189ec20883fc981175f9a03
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-01-22 22:16:30 +0000

    Wrote some JSX to build out the movie details section

--- src/App.js
+++ src/App.js
@@ -265,7 +265,8 @@ const MovieDetails = ({ selectedId, onCloseMovie }) => {
     Genre: genre,
   } = movie;
 
-  console.log(title, year);
+  console.log(title, year, actors);
+  console.log(typeof actors);
 
   useEffect(() => {
     const getMovieDetails = async () => {
@@ -281,10 +282,31 @@ const MovieDetails = ({ selectedId, onCloseMovie }) => {
 
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${movie} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 };
